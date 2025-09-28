c7a0e938b5e17160b66ca0f3e7d5f69f1e7d23d8
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-09-28 21:39:03 +0200

    Add Borrowing model with borrow_date, expected_return, actual_return_date, book_id and user_id & apply migrations

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return = models.DateField()
+    actual_return_date = models.DateField()
+    book_id = models.IntegerField()
+    user_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.borrow_date} - {self.expected_return}"
