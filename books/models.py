ee920f6d40df16d1d9ccb300d98f868509f380ac
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-09-28 17:32:29 +0200

    Add Book model with title, author, cover, inventory and daily_fee & apply initial migrations

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        SOFT = ("SOFT",)
+        HARD = ("HARD",)
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(choices=Cover.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
