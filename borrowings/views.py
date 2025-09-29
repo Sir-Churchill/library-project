b50f430f7d9b97a98b909f578f51a9a5a8004cea
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-09-29 22:34:54 +0200

    Add BorrowingView with create, list, retrieve and filtering logic

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.db import transaction
+from rest_framework import status, mixins, viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
-# Create your views here.
+from books.models import Book
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
+
+
+class BorrowingView(
+    viewsets.GenericViewSet,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = BorrowingSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        book_id = serializer.validated_data["book"].id
+        book = Book.objects.select_for_update().get(pk=book_id)
+        with transaction.atomic():
+            if book.inventory <= 0:
+                raise ValidationError("This book is out of stock")
+
+            book.inventory -= 1
+            book.save()
+            serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        elif self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingSerializer
+
+    @staticmethod
+    def _params_to_ints(qs):
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        user_id = self.request.query_params.get("user_id")
+        is_active = self.request.query_params.get("is_active")
+
+        if user_id:
+            user_ids = self._params_to_ints(user_id)
+            queryset = queryset.filter(user_id__in=user_ids)
+
+        if is_active:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
+        return queryset
