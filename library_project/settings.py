c5853d463d9843a0cebf194f932a6f9113f8f200
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-09-30 02:11:45 +0200

    Configure REST_FRAMEWORK with JWT auth, custom permissions and AutoSchema

--- library_project/settings.py
+++ library_project/settings.py
@@ -129,3 +129,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "books.permissions.IsAdminOrAuthenticatedOrReadOnly"
+    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
