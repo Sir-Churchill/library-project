ccefe5e610d15ca62fbd4410c0d1ccd8c93f1b2f
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-10-02 18:55:45 +0200

    Replace IntegerFields with ForeignKey to User and ManyToManyField to Book

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+from books.models import Book
+from library_project import settings
+
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return = models.DateField()
     actual_return_date = models.DateField(blank=True, null=True)
-    book_id = models.IntegerField()
-    user_id = models.IntegerField()
+    book = models.ManyToManyField(Book)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.borrow_date} - {self.expected_return}"
