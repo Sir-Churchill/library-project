dd34f5d9f33fe9a2d854693d871c6f3cf946b49c
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-10-25 19:28:03 +0200

    Add drf-spectacular schema and Swagger/Redoc endpoints

--- library_project/urls.py
+++ library_project/urls.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("api/v1/", include("books.urls", namespace="books")),
     path("api/v2/", include("borrowings.urls", namespace="borrowings")),
     path("api/users/", include("users.urls", namespace="users")),
     path("api/payments/", include("payment.urls", namespace="payment")),
+    path("api/shema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="schema-swagger",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="schema-redoc",
+    ),
     path("admin/", admin.site.urls),
 ]
